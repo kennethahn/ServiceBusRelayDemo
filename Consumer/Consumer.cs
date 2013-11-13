ed6f5b3da036143bf6575207fac4be045e6203bb
Kenneth Ahn Jensen <kj@tmnet.dk>
2013-11-13 15:46:00 +0100

    Producer and consumer communicates ok

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Consumer
+    {
+        private static string queueName = "greetings";
+        private static QueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            CreateQueue();
+            queueClient = QueueClient.Create(queueName);
+            try
+            {
+                while (true)
+                {
+                    Console.Out.WriteLine("Ready for messages...");
+                    var msg = queueClient.Receive(new TimeSpan(1, 0, 0));
+                    Console.Out.WriteLine("Received message with id {0}", msg.MessageId);
+                    Console.Out.WriteLine("Message is: {0}", msg.GetBody<string>());
+                }
+            }
+            finally
+            {
+                queueClient.Close();
+            }
+        }
+
+        private static void CreateQueue()
+        {
+            NamespaceManager nsMgr = NamespaceManager.Create();
+            if (!nsMgr.QueueExists(queueName))
+            {
+                Console.Out.Write("Creating queue {0}... ", queueName);
+                nsMgr.CreateQueue(queueName);
+                Console.Out.WriteLine("Done.");
+            }
+        }
+    }
+}
---
+++ Producer/Producer.cs
@@ -0,0 +1,63 @@
+﻿using Messages;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Producer
+    {
+        private static string queueName = "greetings";
+        private static QueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            queueClient = QueueClient.Create(queueName);
+            bool keepOn = true;
+            while(keepOn)
+            {
+                Console.Out.WriteLine("Who do you want to send a message to?");
+                string name = Console.ReadLine();
+                Console.Out.WriteLine("What is your message to " + name + "?");
+                string message = Console.ReadLine();
+                SendMessage(name, message);
+                Console.Out.WriteLine("Press [ENTER] to send another message or any other key to quit");
+                ConsoleKeyInfo ki = Console.ReadKey();
+                keepOn = (ki.Key == ConsoleKey.Enter);
+            }
+            queueClient.Close();
+        }
+
+        private static void SendMessage(string name, string message)
+        {
+            Greeting g = new Greeting() { Name = name, Message = message };
+            while(true){
+                try
+                {
+                    BrokeredMessage bmsg = new BrokeredMessage(message);
+                    queueClient.Send(bmsg);
+                    Console.Out.WriteLine("Sent message with id {0}", bmsg.MessageId);
+                    break;
+                }
+                catch (MessagingException mex)
+                {
+                    if (!mex.IsTransient)
+                    {
+                        throw;
+                    }
+                    else
+                    {
+                        Console.Out.WriteLine("We experienced a temporary setback due to {0}", mex.Message);
+                        Console.Out.WriteLine("Retrying in 2 seconds.");
+                        Thread.Sleep(2000);
+                    }
+                }
+            }
+        }
+    }
+}
