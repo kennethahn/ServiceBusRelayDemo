a81a2243ba1149dfea7c58dea99b0df5154abf4f
Kenneth Ahn Jensen <kj@tmnet.dk>
2013-12-06 15:30:39 +0100

    messages now get deleted after being received

--- Consumer/Consumer.cs
+++ Consumer/Consumer.cs
@@ -12,19 +12,28 @@ namespace Consumer
     {
         private static string queueName = "greetings";
         private static QueueClient queueClient;
+        private static NamespaceManager nsMgr;
 
         static void Main(string[] args)
         {
             CreateQueue();
-            queueClient = QueueClient.Create(queueName);
+            queueClient = QueueClient.Create(queueName, ReceiveMode.PeekLock);
+            Console.Out.WriteLine("Queue client receive mode is " + queueClient.Mode.ToString());
             try
             {
                 while (true)
                 {
-                    Console.Out.WriteLine("Ready for messages...");
+                    long msgCount = nsMgr.GetQueue(queueName).MessageCountDetails.ActiveMessageCount;
+                    Console.Out.WriteLine("Ready for messages... ");
+                    if (msgCount > 0) { Console.Out.WriteLine("The queue has " + msgCount + " messages waiting"); }
                     var msg = queueClient.Receive(new TimeSpan(1, 0, 0));
                     Console.Out.WriteLine("Received message with id {0}", msg.MessageId);
                     Console.Out.WriteLine("Message is: {0}", msg.GetBody<string>());
+                    Console.Out.WriteLine("Message is locked with token: {0}", msg.LockToken.ToString());
+                    if (queueClient.Mode == ReceiveMode.PeekLock)
+                    {
+                        queueClient.Complete(msg.LockToken);
+                    }
                 }
             }
             finally
@@ -35,7 +44,10 @@ namespace Consumer
 
         private static void CreateQueue()
         {
-            NamespaceManager nsMgr = NamespaceManager.Create();
+            if (nsMgr == null)
+            {
+                nsMgr = NamespaceManager.Create();
+            }
             if (!nsMgr.QueueExists(queueName))
             {
                 Console.Out.Write("Creating queue {0}... ", queueName);
